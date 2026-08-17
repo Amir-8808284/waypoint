a5a0a9e540adf0a7af2f92cbfcac514838d60d3e
Amir-8808284 <amirsultan1992@gmail.com>
2026-08-16 21:16:52 -0400

    week 12: add trail model database and admin

---
+++ trails/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+from .models import Trail
+
+
+def catalog(request):
+    trails = Trail.objects.filter(
+        is_open=True
+    ).order_by(
+        "distance_km"
+    )
+
+    context = {
+        "trails": trails
+    }
+
+    return render(
+        request,
+        "catalog.html",
+        context
+    )
